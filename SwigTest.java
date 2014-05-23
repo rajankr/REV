5397f0da72271683841e58f2143adff2234c13bc
rajankr <rajan.kumar@girmiti.com>
2014-05-23 20:08:58 +0530

    First GIT

---
+++ SwigTest.java
@@ -0,0 +1,76 @@
+package org.swig.Test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ScrollView;
+import android.text.method.ScrollingMovementMethod;
+
+public class SwigTest extends Activity
+{
+    TextView outputText = null;
+    ScrollView scroller = null;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        outputText = (TextView)findViewById(R.id.OutputText);
+        outputText.setText("Press 'Run' to start...\n");
+        outputText.setMovementMethod(new ScrollingMovementMethod());
+
+        scroller = (ScrollView)findViewById(R.id.Scroller);
+    }
+
+    public void onRunButtonClick(View view)
+    {
+      outputText.append("Started...\n");
+      nativeCall();
+      outputText.append("Finished!\n");
+      
+      // Ensure scroll to end of text
+      scroller.post(new Runnable() {
+        public void run() {
+          scroller.fullScroll(ScrollView.FOCUS_DOWN);
+        }
+      });
+    }
+	
+/*public class demo
+{
+int a,b,c;
+}*/
+	
+public void nativeCall()
+{
+	
+	demo d = new demo();
+	
+	
+	d=Test.getD();
+	d.setA(5);
+	d.setB(6);
+	d.setC(8);
+	Test.setD(d);
+	//outputText.append("the structure values " +d.a + "  " + d.b + " " + d.c + "\n");
+	//d=Structure.getvalue(d);	
+	//outputText.append("the structure values " +d.a + "  " + d.b + "  " + d.c + "\n");
+	Test.setValue(d);
+	//outputText.append("the structure values " +d.a + "  " + d.b + " " + d.c + "\n");
+	
+
+
+}
+
+ static {
+        System.loadLibrary("Test");
+    }
+
+
+
+}
